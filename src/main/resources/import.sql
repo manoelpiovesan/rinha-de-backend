634c4d4f57bbe7e6adcc1bcb6fb015dfc7e69c85
Manoel <manoelpiovesan@gmail.com>
2024-02-05 11:20:03 -0300

    Adding dataset from import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,15 @@
+--- Customers
+-- 1
+INSERT INTO customer (id, account_limit, balance) values (1, 100000, 0);
+
+-- 2
+INSERT INTO customer (id, account_limit, balance) values (2, 80000, 0);
+
+-- 3
+INSERT INTO customer (id, account_limit, balance) values (3, 1000000, 0);
+
+-- 4
+INSERT INTO customer (id, account_limit, balance) values (4, 10000000, 0);
+
+-- 5
+INSERT INTO customer (id, account_limit, balance) values (5, 500000, 0);
